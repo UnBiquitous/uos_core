51150d6d5efb4d2531b09bb46915c376533a0429
Fabricio Buzeto <nuk.anime13@gmail.com>
2013-04-18 16:53:21 -0400

    ApplicationManager Starts the apps

--- src/br/unb/unbiquitous/ubiquitos/uos/applicationManager/ApplicationManager.java
+++ src/br/unb/unbiquitous/ubiquitos/uos/applicationManager/ApplicationManager.java
@@ -1,40 +1,118 @@
 package br.unb.unbiquitous.ubiquitos.uos.applicationManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
+import br.unb.unbiquitous.ubiquitos.Logger;
+import br.unb.unbiquitous.ubiquitos.uos.adaptabitilyEngine.Gateway;
 import br.unb.unbiquitous.ubiquitos.uos.application.UosApplication;
 import br.unb.unbiquitous.ubiquitos.uos.ontologyEngine.Ontology;
 import br.unb.unbiquitous.ubiquitos.uos.ontologyEngine.exception.ReasonerNotDefinedException;
 
 public class ApplicationManager {
-
-	private List<UosApplication> apps = new ArrayList<UosApplication>();
+	private static final Logger logger = Logger.getLogger(ApplicationManager.class); 
+	
+	private List<UosApplication> toInitialize = new ArrayList<UosApplication>();
+	private Map<String, UosApplication> deployed = new HashMap<String, UosApplication>();
 	private final ResourceBundle properties;
 
-	public ApplicationManager(ResourceBundle properties) {
+	private final Gateway gateway;
+
+	public ApplicationManager(ResourceBundle properties, Gateway gateway) {
 		this.properties = properties;
+		this.gateway = gateway;
 	}
 
 	public void add(UosApplication app) {
-		this.apps.add(app);
+		this.toInitialize.add(app);
 	}
 
 	public void startApplications() {
-		for (UosApplication app : apps) {
-			// Deve criar uma ontologia para cada um usando o properties
-			Ontology ontology;
-			try {
-				ontology = new Ontology(properties);
-				//TODO: mais regras
-				app.init(ontology);
-				app.start(null, null);
-			} catch (ReasonerNotDefinedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		for (final UosApplication app : toInitialize) {
+			//TODO: The ontology rules are not enforced by tests
+			deploy(app);
+		}
+		toInitialize.clear();
+	}
+
+	public void deploy(UosApplication app) {
+		deploy(app,null);
+	}
+	
+	public void deploy(UosApplication app, String id) {
+		initApp(app);
+		startApp(app);
+		deployed.put(id, app);
+	}
+	
+	private void startApp(final UosApplication app) {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				app.start(gateway, createStartOntology());
+			}
+		});
+		t.start();
+	}
+
+	private Ontology createStartOntology() {
+		try {
+			return new Ontology(properties);
+		} catch (ReasonerNotDefinedException ex) {
+			logger.info("Ontology component disabled.");
+			return null;
+		}
+	}
+	
+	private void initApp(final UosApplication app) {
+		Ontology initOntology = createInitOntology(app);
+		app.init(initOntology);
+		if (initOntology != null){
+			initOntology.saveChanges();
+		}
+	}
+
+	private Ontology createInitOntology(UosApplication app) {
+		try {
+			Ontology ontology = new Ontology(properties);
+			if (!ontology.getOntologyDeployInstance().hasInstanceOf(app.getClass().getName(), "application")) {
+		        ontology.getOntologyDeployInstance().addInstanceOf(app.getClass().getName(), "application");
+		        return ontology;
+		    } else {
+		        logger.error("ApplicationClass '" + app.getClass().getName() + " is already deployed.");
+		    }
+		} catch (ReasonerNotDefinedException e) {
+			 logger.info("Ontology component disabled.");
+		}
+		return null;
+	}
+	
+	public void tearDown() throws Exception {
+		for (final UosApplication app : toInitialize) {
+			Ontology ontology = createUndeployOntology(app);
+			app.stop();
+			app.tearDown(ontology);
+			if (ontology != null){
+				ontology.saveChanges();
 			}
 		}
 	}
 
+	private Ontology createUndeployOntology(final UosApplication app) {
+		Ontology ontology;
+		try {
+			ontology = new Ontology(properties);
+			ontology.getOntologyUndeployInstance().removeInstanceOf(
+					app.getClass().getName(), "application");
+		} catch (ReasonerNotDefinedException ex) {
+			ontology = null;
+		}
+		return ontology;
+	}
+
+	public UosApplication findApplication(String id) {
+		return deployed.get(id);
+	}
 }
--- srcTest/br/unb/unbiquitous/ubiquitos/uos/applicationManager/ApplicationManagerTest.java
+++ srcTest/br/unb/unbiquitous/ubiquitos/uos/applicationManager/ApplicationManagerTest.java
@@ -1,10 +1,14 @@
 package br.unb.unbiquitous.ubiquitos.uos.applicationManager;
 
-import static org.fest.assertions.api.Assertions.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
+import java.io.File;
 import java.util.ListResourceBundle;
 import java.util.ResourceBundle;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,8 +21,11 @@ import br.unb.unbiquitous.ubiquitos.uos.ontologyEngine.api.OntologyUndeploy;
 public class ApplicationManagerTest {
 
 	private ApplicationManager manager;
+	private Gateway gateway;
 
-	@Before public void setUp(){
+	@Before public void setUp() throws Exception{
+		gateway = mock(Gateway.class);
+		new File("resources/owl/uoscontext.owl").createNewFile();
 		ResourceBundle bundle = new ListResourceBundle() {
 			protected Object[][] getContents() {
 				return new Object[][] {
@@ -27,7 +34,22 @@ public class ApplicationManagerTest {
 				};
 			}
 		};
-		manager = new ApplicationManager(bundle);
+		manager = new ApplicationManager(bundle,gateway);
+	}
+	
+	private void createOntologyDisabledManager() {
+		ResourceBundle bundle = new ListResourceBundle() {
+			protected Object[][] getContents() {
+				return new Object[][] {
+						{"ubiquitos.ontology.path","resources/owl/uoscontext.owl"},
+				};
+			}
+		};
+		manager = new ApplicationManager(bundle,gateway);
+	}
+	
+	@After public void tearDown(){
+		new File("resources/owl/uoscontext.owl").delete();
 	}
 	
 	@Test public void addingAnApplicationDoesNothingToIt(){
@@ -39,12 +61,14 @@ public class ApplicationManagerTest {
 		assertThat(app.finished).isFalse();
 	}
 	
-	@Test public void startInitsAndStartsAllApplications(){
-		DummyApp app1 = new DummyApp();
-		DummyApp app2 = new DummyApp();
+	@Test public void startInitsAndStartsAllApplications() throws InterruptedException{
+		final DummyApp app1 = new DummyApp();
+		final DummyApp app2 = new DummyApp();
 		manager.add(app1);
 		manager.add(app2);
 		manager.startApplications();
+		waitToStart(app1);
+		waitToStart(app2);
 		assertThat(app1.inited).isTrue();
 		assertThat(app1.started).isTrue();
 		assertThat(app1.stoped).isFalse();
@@ -55,6 +79,16 @@ public class ApplicationManagerTest {
 		assertThat(app2.finished).isFalse();
 	}
 	
+	@Test public void startInitsAndStartsAllApplicationsOnlyOnce() throws InterruptedException{
+		final DummyApp app = new DummyApp();
+		manager.add(app);
+		manager.startApplications();
+		manager.startApplications();
+		waitToStart(app);
+		assertThat(app.initedCount).isEqualTo(1);
+		assertThat(app.startedCount).isEqualTo(1);
+	}
+	
 	@Test public void initsTheAppWithTheProperOntology(){
 		DummyApp app = new DummyApp();
 		manager.add(app);
@@ -62,29 +96,146 @@ public class ApplicationManagerTest {
 		assertThat(app.initOntology).isNotNull();
 	}
 	
+	@Test public void initsTheAppWithNoOntologyWhenDisabled(){
+		createOntologyDisabledManager();
+		DummyApp app = new DummyApp();
+		manager.add(app);
+		manager.startApplications();
+		assertThat(app.initOntology).isNull();
+	}
+
+	@Test public void startsTheAppWithTheProperOntologyAndGateway() throws InterruptedException{
+		final DummyApp app = new DummyApp();
+		manager.add(app);
+		manager.startApplications();
+		waitToStart(app);
+		assertThat(app.startOntology).isNotNull();
+		assertThat(app.gateway).isSameAs(gateway);
+	}
 	
-	//add
-	//start
-	//deploy
+	@Test public void startsTheAppWithNoOntologyWhenDisableButStillWithGateway() throws InterruptedException{
+		createOntologyDisabledManager();
+		final DummyApp app = new DummyApp();
+		manager.add(app);
+		manager.startApplications();
+		waitToStart(app);
+		assertThat(app.startOntology).isNull();
+		assertThat(app.gateway).isSameAs(gateway);
+	}
+	
+	@Test public void startsTheAppInADifferentThread() throws InterruptedException{
+		final DummyApp app = new DummyApp(){
+			@Override
+			public void start(Gateway gateway, OntologyStart ontology) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {}
+				super.start(gateway, ontology);
+			}
+		};
+		manager.add(app);
+		manager.startApplications();
+		Thread.sleep(50);
+		assertThat(app.started).isFalse();
+		waitToStart(app);
+	}
+	
+	@Test public void deployDoesInitsAndStarts() throws InterruptedException{
+		final DummyApp app = new DummyApp();
+		manager.deploy(app);
+		waitToStart(app);
+		assertThat(app.inited).isTrue();
+		assertThat(app.started).isTrue();
+		assertThat(app.stoped).isFalse();
+		assertThat(app.finished).isFalse();
+	}
+	
+	@Test public void tearDownStopsAndTearDownAllApplications() throws Exception{
+		final DummyApp app1 = new DummyApp();
+		final DummyApp app2 = new DummyApp();
+		manager.add(app1);
+		manager.add(app2);
+		manager.tearDown();
+		assertThat(app1.stoped).isTrue();
+		assertThat(app1.finished).isTrue();
+		assertThat(app2.stoped).isTrue();
+		assertThat(app2.finished).isTrue();
+	}
+	
+	@Test public void tearsDownTheAppWithTheProperOntology() throws Exception{
+		final DummyApp app = new DummyApp();
+		manager.add(app);
+		manager.tearDown();
+		assertThat(app.teardownOntology).isNotNull();
+	}
+	
+	@Test public void tearsDownTheAppWithNoOntologyWhenDisable() throws Exception{
+		createOntologyDisabledManager();
+		final DummyApp app = new DummyApp();
+		manager.add(app);
+		manager.tearDown();
+		assertThat(app.teardownOntology).isNull();
+	}
+	
+	@Test public void findDeployedApps() throws Exception{
+		DummyApp app1 = new DummyApp();
+		manager.deploy(app1,"app1");
+		DummyApp app2 = new DummyApp();
+		manager.deploy(app2,"app2");
+		assertThat(manager.findApplication("app1")).isSameAs(app1);
+	}
+
+	private void waitToStart(final DummyApp app) throws InterruptedException {
+		assertEventuallyTrue("Must start a Thread with the app", 1000, 
+				new EventuallyAssert() {
+					public boolean assertion() {
+						return app.started;
+					}
+				});
+	}
+	
+	static interface EventuallyAssert{
+		boolean assertion();
+	}
+	
+	private void assertEventuallyTrue(String msg, long wait, EventuallyAssert assertion) throws InterruptedException{
+		long time = 0;
+		while (time <= wait && !assertion.assertion()){
+			Thread.sleep(10);
+			time += 10;
+		}
+		assertTrue(msg,assertion.assertion());
+	}
+	
+	
 	
 }
 
 class DummyApp implements UosApplication{
 	
-	boolean started;
-	boolean stoped;
 	boolean inited;
+	int	initedCount;
+	boolean started;
+	int	startedCount;
+	boolean stoped;
 	boolean finished;
 	OntologyDeploy initOntology;
+	OntologyStart startOntology;
+	OntologyUndeploy teardownOntology;
+	Gateway gateway;
 	
 	@Override
 	public void init(OntologyDeploy ontology) {
 		this.initOntology = ontology;
 		inited = true;
+		initedCount ++;
 	}
 	
 	public void start(Gateway gateway, OntologyStart ontology) {
+		this.gateway = gateway;
+		this.startOntology = ontology;
 		started= true;
+		startedCount++;
 	}
 
 	@Override
@@ -94,6 +245,7 @@ class DummyApp implements UosApplication{
 
 	@Override
 	public void tearDown(OntologyUndeploy ontology) throws Exception {
+		this.teardownOntology = ontology;
 		finished = true;
 	}
 	
